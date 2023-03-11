cc8e9a4ebfd8f1a9bcb61f04a12d01b768c2a949
ddbb <dragoncultxyz@gmail.com>
2023-03-11 18:52:09 +0700

    v0.2 on github

---
+++ precache-manifest.6a175976b5af6ec2c9fc3ac08efc5176.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b52f53c63c4f5fcc2ecbcf8fbb0be71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "824efa4d82508586a358",
+    "url": "/static/css/2.3d433e25.chunk.css"
+  },
+  {
+    "revision": "1cfe44f967e58ba5ff56",
+    "url": "/static/css/main.70568ac4.chunk.css"
+  },
+  {
+    "revision": "824efa4d82508586a358",
+    "url": "/static/js/2.e231c2d6.chunk.js"
+  },
+  {
+    "revision": "d574ea148687c7831aa7f45d136c98d8",
+    "url": "/static/js/2.e231c2d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cfe44f967e58ba5ff56",
+    "url": "/static/js/main.8749f789.chunk.js"
+  },
+  {
+    "revision": "3f8b7f4c04e766f3b98e",
+    "url": "/static/js/runtime-main.3486abd2.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/FontAwesome.b06871f2.ttf"
+  }
+]);
